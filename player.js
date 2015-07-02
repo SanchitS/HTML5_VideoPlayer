be603fd3e9d2dbb016591fac27fc1de6b0beeed3
sanchits <sanchitsharma1209@gmail.com>
2015-07-02 16:12:08 +0530

    adding pause screen

--- player.js
+++ player.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function () {
 
     //video container
     video = document.getElementById('video');
+    pauseScreen = document.getElementById('screen');
+    screenButton = document.getElementById('screen-button');
 
     //progress bar container
     pbar = document.getElementById('pbar');
@@ -25,6 +27,7 @@ window.addEventListener('load', function () {
         soundButton.addEventListener('click', MuteOrUnmute, false);
         sbarContainer.addEventListener('click', changeVolume, false);
         fullscreenButton.addEventListener('click', fullscreen, false);
+        screenButton.addEventListener('click', playOrPause, false);
 
     }, false);
 
@@ -35,10 +38,16 @@ function playOrPause() {
         video.play();
         playButton.src = 'images/pause.png';
         update = setInterval(updatePlayer, 30);
+        pauseScreen.style.display = 'none';
+        screenButton.src = 'images/play.png';
+
     } else {
         video.pause();
         playButton.src = 'images/play.png';
         window.clearInterval(update);
+        pauseScreen.style.display = 'block';
+        screenButton.src = 'images/play.png';
+
     }
 }
 
@@ -49,6 +58,12 @@ function updatePlayer() {
     if (video.ended) {
         window.clearInterval(update);
         playButton.src = 'images/replay.png';
+        pauseScreen.style.display = 'block';
+        screenButton.src = 'images/replay.png';
+    } else if (video.paused) {
+        playButton.src = 'images/play.png';
+        screenButton.src = 'images/play.png';
+
     }
 }
 
